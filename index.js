9aedc305e1c89c4f39c6e478c09bb9a28a2a69a5
Jonathan Zúñiga <jonathanzuniga@gmail.com>
2020-09-25 12:28:09 -0700

    Handling HTTP PUT Requests

--- index.js
+++ index.js
@@ -18,15 +18,19 @@ app.get('/api/courses', (req, res) => {
     res.send(courses)
 })
 
+app.get('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id))
+
+    if (!course) res.status(404).send('The course with the given ID was not found.')
+
+    res.send(course)
+})
+
 app.post('/api/courses', (req, res) => {
-    const schema = {
-        name: Joi.string().min(3).required()
-    }
+    const { error } = validateCourse(req.body)
 
-    const result = Joi.validate(req.body, schema)
-
-    if (result.error) {
-        res.status(400).send(result.error.details[0].message)
+    if (error) {
+        res.status(400).send(error.details[0].message)
         return
     }
 
@@ -45,13 +49,34 @@ app.post('/api/courses', (req, res) => {
     res.send(course)
 })
 
-app.get('/api/courses/:id', (req, res) => {
+app.put('/api/courses/:id', (req, res) => {
+    // Look up the course
+    // If not existing, return 404
     const course = courses.find(c => c.id === parseInt(req.params.id))
 
     if (!course) res.status(404).send('The course with the given ID was not found.')
 
+    // Validate
+    // If invalid, return 400 - Bad request
+    const { error } = validateCourse(req.body)
+
+    if (error) {
+        res.status(400).send(error.details[0].message)
+        return
+    }
+
+    // Update course
+    // Return the updated course
+    course.name = req.body.name
     res.send(course)
 })
+
+function validateCourse(course) {
+    const schema = {
+        name: Joi.string().min(3).required()
+    }
+    return Joi.validate(course, schema)
+}
 
 // run command line to set a env var:
 // mac: export PORT=5000

dbde284b1e660576de866427205b09608d80fa05
Jonathan Zúñiga <jonathanzuniga@gmail.com>
2020-09-25 10:49:46 -0700

    Handling HTTP GET Requests

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const express = require('express')
 const app = express()
 
+const courses = [
+    {id: 1, name: 'Course 1'},
+    {id: 2, name: 'Course 2'},
+    {id: 3, name: 'Course 3'}
+]
+
 app.get('/', (req, res) => {
     res.send('Hello World!!!')
 })
 
 app.get('/api/courses', (req, res) => {
-    res.send([1, 2, 3])
+    res.send(courses)
 })
 
 app.get('/api/courses/:id', (req, res) => {
-    res.send(req.params.id)
+    const course = courses.find(c => c.id === parseInt(req.params.id))
+
+    if (!course) res.status(404).send('The course with the given ID was not found.')
+
+    res.send(course)
 })
 
 // run command line to set a env var:

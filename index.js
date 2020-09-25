6b3bd6cd8758639c437eb08043178b2b3fc9b803
Jonathan Zúñiga <jonathanzuniga@gmail.com>
2020-09-25 16:04:24 -0700

    Handling HTTP DELETE Requests

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get('/api/courses', (req, res) => {
 app.get('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id))
 
-    if (!course) res.status(404).send('The course with the given ID was not found.')
+    if (!course) return res.status(404).send('The course with the given ID was not found.')
 
     res.send(course)
 })
@@ -29,10 +29,7 @@ app.get('/api/courses/:id', (req, res) => {
 app.post('/api/courses', (req, res) => {
     const { error } = validateCourse(req.body)
 
-    if (error) {
-        res.status(400).send(error.details[0].message)
-        return
-    }
+    if (error) return res.status(400).send(error.details[0].message)
 
     // if (!req.body.name || req.body.name.length < 3) {
     //     // 400 Bad Request
@@ -54,16 +51,13 @@ app.put('/api/courses/:id', (req, res) => {
     // If not existing, return 404
     const course = courses.find(c => c.id === parseInt(req.params.id))
 
-    if (!course) res.status(404).send('The course with the given ID was not found.')
+    if (!course) return res.status(404).send('The course with the given ID was not found.')
 
     // Validate
     // If invalid, return 400 - Bad request
     const { error } = validateCourse(req.body)
 
-    if (error) {
-        res.status(400).send(error.details[0].message)
-        return
-    }
+    if (error) return res.status(400).send(error.details[0].message)
 
     // Update course
     // Return the updated course
@@ -77,6 +71,21 @@ function validateCourse(course) {
     }
     return Joi.validate(course, schema)
 }
+
+app.delete('/api/courses/:id', (req, res) => {
+    // Look up the course
+    // Not existing, return 404
+    const course = courses.find(c => c.id === parseInt(req.params.id))
+
+    if (!course) return res.status(404).send('The course with the given ID was not found.')
+
+    // Delete
+    const index = courses.indexOf(course)
+    courses.splice(index, 1)
+
+    // Return the same course
+    res.send(course)
+})
 
 // run command line to set a env var:
 // mac: export PORT=5000
